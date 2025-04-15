e913ea210f0b9e7f352b1c468b65bc41bc7b4b8d
roma5525 <147617588+roma5525@users.noreply.github.com>
2025-04-15 17:44:27 +0700

    Add files via upload

---
+++ INO/water.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+#include "mcp3021.h"
+byte ADDR;
+// (также попробуйте просканировать адрес: https://github.com/MAKblC/Codes/tree/master/I2C%20scanner)
+// 0x48 (000)...0x4F(111)
+
+MCP3021 mcp3021;
+
+// калибровочные значения с АЦП
+const float air_value = 804.0;
+const float water_value = 420.0;
+const float moisture_0 = 0.0;
+const float moisture_100 = 100.0;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация I2C интерфейса
+  Wire.begin();
+  ADDR = scan(); // скан адреса
+  Serial.println(" АДРЕС:");
+  Serial.println(ADDR, HEX);
+  // Инициализация АЦП
+  ADDR = ADDR-0x48;
+  mcp3021.begin(ADDR);
+}
+
+void loop() {
+  // Измерение
+  float adc0 = mcp3021.readADC();
+  float h = map(adc0, air_value, water_value, moisture_0, moisture_100);
+  // Вывод измеренных значений в терминал
+  Serial.println("Water level = " + String(h, 1) + " %");
+  delay(250);
+}
+
+
+byte scan() { // сканирование адреса
+  byte error, address;
+  Serial.println("Сканирую...");
+  for (address = 8; address < 127; address++ ) {
+	Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C устройство найдено по адресу 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      return address;
+      Serial.println(" !");
+      break;
+    }
+  }
+}
