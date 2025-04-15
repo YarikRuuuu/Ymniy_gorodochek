d7fb8112c043e2d5bca36163bed29ed07da2f314
roma5525 <147617588+roma5525@users.noreply.github.com>
2025-04-15 12:43:33 +0700

    Add files via upload

---
+++ Distance.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <VL53L0X.h>
+VL53L0X lox;
+
+void setup() {
+  // Инициализация последовательного порта
+  Serial.begin(9600);
+  // Инициализация датчика
+  Wire.begin();
+  lox.init();
+  lox.setTimeout(500);
+  // параметры для режима высокой точности
+  lox.setMeasurementTimingBudget(200000);
+}
+
+void loop() {
+  // Измерение
+  float dist = lox.readRangeSingleMillimeters();
+  // Вывод измеренных значений в терминал
+  Serial.println("Distance  = " + String(dist, 0) + " mm  ");
+  delay(250);
+}
