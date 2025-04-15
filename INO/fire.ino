c9db3e64dfe89f576e99e375879d253adc1db1b8
roma5525 <147617588+roma5525@users.noreply.github.com>
2025-04-15 20:01:54 +0500

    Add files via upload

---
+++ INO/fire.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <MGS_FR403.h>
+MGS_FR403 Fire;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Fire.begin();
+}
+
+void loop() {
+  Fire.get_ir_and_vis();
+  Serial.print("ИК: ");
+  Serial.println(String(Fire.ir_data, 1));
+  Serial.print("Видимый: ");
+  Serial.println(String(Fire.vis_data, 1));
+  delay(500);
+}
